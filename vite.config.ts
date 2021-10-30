a87aceab98876f18bf88884c50a934246c00b4bf
Saverio Tassinari <saverio.tassinari@gmail.com>
2021-10-30 18:32:59 +0100

    more stock than blog described

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
-  build: {
-    outDir: "build",
-  },
   plugins: [reactRefresh(), svgr()],
 });
