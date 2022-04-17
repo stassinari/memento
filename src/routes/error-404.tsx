92bfa4e53be1d7c1d87e3e1c9df4f562cbda1c0c
Saverio Tassinari <saverio.tassinari@gmail.com>
2022-04-17 23:29:56 +0100

    404 image to svg

--- src/routes/error-404.tsx
+++ src/routes/error-404.tsx
@@ -1,9 +1,7 @@
-import React, { FunctionComponent } from "react";
 import { Box, Typography } from "@mui/material";
-
-import makeStyles from '@mui/styles/makeStyles';
-
-import stain from "../static/coffee-stain.png";
+import makeStyles from "@mui/styles/makeStyles";
+import React, { FunctionComponent } from "react";
+import { SpiltCoffee } from "../components/icons/SpiltCoffee";
 import Layout from "../components/layout";
 
 const useStyles = makeStyles(() => {
@@ -18,8 +16,17 @@ const Error404: FunctionComponent = () => {
   const classes = useStyles();
   return (
     <Layout title="Not found">
-      <Box textAlign="center" mt={8}>
-        <img src={stain} alt="Coffee stain" className={classes.image} />
+      <Box textAlign="center" mt={12}>
+        <div
+          style={{
+            width: "50%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginBottom: "48px",
+          }}
+        >
+          <SpiltCoffee />
+        </div>
         <Typography variant="h1">404</Typography>
         <Typography variant="subtitle1" gutterBottom>
           The page you're looking for doesn't exist.
