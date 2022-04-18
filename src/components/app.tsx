93018b0112713483d71973a94d64b2993439b959
Saverio Tassinari <saverio.tassinari@gmail.com>
2022-04-18 22:09:52 +0100

    new react router setup

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,3 +1,28 @@
+import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
+
 export const App = () => {
-  return <div>Hic sunt (React XVIII) lolz</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="lolz" element={<div>Hic sunt (React XVIII) lolz</div>} />
+          <Route path="gags" element={<div>Hic sunt brutte gags</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+const Layout = () => {
+  return (
+    <div>
+      <h1>Hic sunt React XVIII!</h1>
+      <nav>
+        <Link to="lolz">Lolz</Link> | <Link to="gags">Gags</Link>
+      </nav>
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
+  );
 };
