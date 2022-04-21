df7256302734f50692dc382b3f632eafeff611e2
Saverio Tassinari <saverio.tassinari@gmail.com>
2022-04-21 22:46:11 +0100

    wip offline support

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,14 +1,41 @@
 import { css } from "@emotion/react";
-import { getFirestore } from "firebase/firestore";
+import {
+  enableIndexedDbPersistence,
+  initializeFirestore,
+} from "firebase/firestore";
 import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
-import { FirestoreProvider, useFirebaseApp } from "reactfire";
+import { FirestoreProvider, useInitFirestore } from "reactfire";
 import tw from "twin.macro";
 import { Lolz } from "../pages/Lolz";
 
 export const App = () => {
-  const firestoreInstance = getFirestore(useFirebaseApp());
+  const { status, data: firestoreInstance } = useInitFirestore(
+    async (firebaseApp) => {
+      const db = initializeFirestore(firebaseApp, {});
+      try {
+        await enableIndexedDbPersistence(db);
+        return db;
+      } catch (err: any) {
+        if (err.code === "failed-precondition") {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          console.log("failed-precondition");
+        } else if (err.code === "unimplemented") {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          console.log("unimplemented");
+        }
+      } finally {
+        return db;
+      }
+    }
+  );
 
-  // enableIndexedDbPersistence(db).catch(function (err) {
+  if (status === "loading") {
+    return <div>Initializing...</div>;
+  }
+
+  // enableIndexedDbPersistence(firestoreInstance).catch(function (err) {
   //   if (err.code === "failed-precondition") {
   //     // Multiple tabs open, persistence can only be enabled
   //     // in one tab at a a time.
