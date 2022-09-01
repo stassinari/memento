2f892236568d0e31f75b6bcf623aacac7b64b110
Saverio Tassinari <saverio.tassinari@gmail.com>
2022-09-01 20:15:34 +0200

    better logic

--- src/components/hooks/useActiveRoute.tsx
+++ src/components/hooks/useActiveRoute.tsx
@@ -3,11 +3,9 @@ import { useLocation } from "react-router-dom";
 export const useActiveRoute = (linkTo: string) => {
   const { pathname } = useLocation();
 
-  if (linkTo === "/" && pathname === linkTo) {
-    return true;
-  } else if (linkTo !== "/") {
+  if (linkTo !== "/") {
     return pathname.startsWith(linkTo);
   } else {
-    return false;
+    return pathname === linkTo;
   }
 };
