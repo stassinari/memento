3fb337b445e204ccbc3e9c99a7a3869559f8df42
Saverio Tassinari <saverio.tassinari@gmail.com>
2022-11-07 23:08:33 +0000

    fixed empty suggestion

--- src/components/form/FormSuggestions.tsx
+++ src/components/form/FormSuggestions.tsx
@@ -12,7 +12,10 @@ interface FormSuggestionsProps {
 }
 
 export function extractSuggestions<T>(arr: T[], field: keyof T, limit = 3) {
-  return [...new Set(arr.map((b) => b[field]))].slice(0, limit) as string[];
+  return [...new Set(arr.map((b) => b[field]).filter((f) => !!f))].slice(
+    0,
+    limit
+  ) as string[];
 }
 export const FormSuggestions: React.FC<FormSuggestionsProps> = ({
   suggestions,
