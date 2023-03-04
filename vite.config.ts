16e9b68d36a95383939aed327a5dcf3e2d89dab6
Saverio Tassinari <saverio.tassinari@gmail.com>
2023-03-04 16:41:54 +0000

    removed @vitejs/plugin-basic-ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
@@ -6,7 +5,6 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    basicSsl(),
     react({
       jsxImportSource: "@emotion/react",
       babel: {
@@ -62,16 +60,6 @@ export default defineConfig({
         ],
       },
     }),
-    // FIXME revisit when looking into Firebase notifications
-    // VitePWA({
-    //   strategies: "injectManifest",
-    //   srcDir: "src",
-    //   filename: "firebase-messaging-sw.js",
-    //   workbox: {
-    //     globPatterns: [],
-    //     globIgnores: ["*"],
-    //   },
-    // }),
   ],
   esbuild: {
     logOverride: { "this-is-undefined-in-esm": "silent" },
