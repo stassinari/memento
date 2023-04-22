b67da3a31f28bbf1b29e221c60d26ae138c01419
Saverio Tassinari <saverio.tassinari@gmail.com>
2023-04-22 16:58:53 +0100

    enable sorting

--- src/pages/brews/BrewsTable.tsx
+++ src/pages/brews/BrewsTable.tsx
@@ -1,13 +1,16 @@
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/20/solid";
 import {
+  SortingState,
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import dayjs from "dayjs";
 import { orderBy } from "firebase/firestore";
-import { useMemo } from "react";
-import "twin.macro";
+import { useMemo, useState } from "react";
+import tw from "twin.macro";
 import { useCollectionQuery } from "../../hooks/firestore/useCollectionQuery";
 import { useFirestoreCollectionOneTime } from "../../hooks/firestore/useFirestoreCollectionOneTime";
 import { Beans } from "../../types/beans";
@@ -99,6 +102,8 @@ interface BrewsTableProps {
 const BrewsTable: React.FC<BrewsTableProps> = ({ brewsList, beansList }) => {
   console.log("BrewsTable");
 
+  const [sorting, setSorting] = useState<SortingState>([]);
+
   const data = useMemo(
     () =>
       brewsList.map((brew) => ({
@@ -115,7 +120,10 @@ const BrewsTable: React.FC<BrewsTableProps> = ({ brewsList, beansList }) => {
   const table = useReactTable({
     data,
     columns,
+    state: { sorting },
+    onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
   });
 
   return (
@@ -128,20 +136,55 @@ const BrewsTable: React.FC<BrewsTableProps> = ({ brewsList, beansList }) => {
                 <thead tw="bg-gray-50">
                   {table.getHeaderGroups().map((headerGroup) => (
                     <tr key={headerGroup.id}>
-                      {headerGroup.headers.map((header) => (
-                        <th
-                          key={header.id}
-                          scope="col"
-                          tw="py-3.5 px-3 text-left text-sm font-semibold text-gray-900 first-of-type:(pl-4 pr-3 sm:pl-6) last-of-type:(pl-3 pr-4 sm:pr-6)"
-                        >
-                          {header.isPlaceholder
+                      {headerGroup.headers.map((header) => {
+                        const isSorted = header.column.getIsSorted();
+                        console.log(isSorted);
+
+                        return (
+                          <th
+                            key={header.id}
+                            scope="col"
+                            tw="py-3.5 px-3 text-left text-sm font-semibold text-gray-900 first-of-type:(pl-4 pr-3 sm:pl-6) last-of-type:(pl-3 pr-4 sm:pr-6)"
+                          >
+                            {header.isPlaceholder ? null : (
+                              <div
+                                css={tw`inline-flex`}
+                                {...{
+                                  className: header.column.getCanSort()
+                                    ? "group cursor-pointer select-none"
+                                    : "group",
+                                  onClick:
+                                    header.column.getToggleSortingHandler(),
+                                }}
+                              >
+                                {flexRender(
+                                  header.column.columnDef.header,
+                                  header.getContext()
+                                )}
+                                <span className="flex-none ml-2 text-gray-900 bg-gray-200 rounded group-hover:bg-gray-300">
+                                  {header.column.getIsSorted() === "asc" ? (
+                                    <ChevronUpIcon
+                                      className="w-5 h-5"
+                                      aria-hidden="true"
+                                    />
+                                  ) : header.column.getIsSorted() === "desc" ? (
+                                    <ChevronDownIcon
+                                      className="w-5 h-5"
+                                      aria-hidden="true"
+                                    />
+                                  ) : null}
+                                </span>
+                              </div>
+                            )}
+                            {/* {header.isPlaceholder
                             ? null
                             : flexRender(
                                 header.column.columnDef.header,
                                 header.getContext()
-                              )}
-                        </th>
-                      ))}
+                              )} */}
+                          </th>
+                        );
+                      })}
                     </tr>
                   ))}
                 </thead>
