eca89a122ab545a87087fcfdb978fbfe6442d732
Saverio Tassinari <saverio.tassinari@gmail.com>
2026-02-16 22:12:36 +0000

    drinks index sql-only

--- src/routes/_auth/_layout/drinks/index.tsx
+++ src/routes/_auth/_layout/drinks/index.tsx
@@ -1,87 +1,45 @@
 import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
 import { Link as RouterLink, createFileRoute } from "@tanstack/react-router";
-import { limit, orderBy } from "firebase/firestore";
-import { useMemo } from "react";
 import { navLinks } from "~/components/BottomNav";
 import { BreadcrumbsWithHome } from "~/components/Breadcrumbs";
 import {
   DrinksList as DrinksListPostgres,
   mergeBrewsAndEspressoByUniqueDate as mergePostgres,
 } from "~/components/drinks/DrinksList";
-import {
-  DrinksList as DrinksListFirebase,
-  mergeBrewsAndEspressoByUniqueDate as mergeFirebase,
-} from "~/components/drinks/DrinksList.Firebase";
 import { Heading } from "~/components/Heading";
 import { Link } from "~/components/Link";
 import { getBrews, getEspressos } from "~/db/queries";
-import { useCollectionQuery } from "~/hooks/firestore/useCollectionQuery";
-import { useFirestoreCollectionRealtime } from "~/hooks/firestore/useFirestoreCollectionRealtime";
 import { useCurrentUser } from "~/hooks/useInitUser";
-import { Beans } from "~/types/beans";
-import { Brew } from "~/types/brew";
-import { Espresso } from "~/types/espresso";
-import { flagsQueryOptions } from "../feature-flags";
-
-type BrewWithBeans = Awaited<ReturnType<typeof getBrews>>[number];
-type EspressoWithBeans = NonNullable<
-  Awaited<ReturnType<typeof getEspressos>>[number]
->;
 
 const brewsQueryOptions = (firebaseUid: string) =>
-  queryOptions<BrewWithBeans[]>({
+  queryOptions({
     queryKey: ["brews", firebaseUid],
-    queryFn: () => getBrews({ data: firebaseUid }) as Promise<BrewWithBeans[]>,
+    queryFn: () => getBrews({ data: { firebaseUid, limit: 30, offset: 0 } }),
   });
 
 const espressosQueryOptions = (firebaseUid: string) =>
-  queryOptions<EspressoWithBeans[]>({
+  queryOptions({
     queryKey: ["espressos", firebaseUid],
     queryFn: () =>
-      getEspressos({ data: firebaseUid }) as Promise<EspressoWithBeans[]>,
+      getEspressos({ data: { firebaseUid, limit: 30, offset: 0 } }),
   });
 
 export const Route = createFileRoute("/_auth/_layout/drinks/")({
   component: DrinksPage,
-  loader: async ({ context }) => {
-    await context.queryClient.ensureQueryData(flagsQueryOptions());
-  },
 });
 
 function DrinksPage() {
   console.log("DrinksPage");
   const user = useCurrentUser();
 
-  const { data: flags } = useSuspenseQuery(flagsQueryOptions());
-  const readFromPostgres = flags?.find(
-    (flag) => flag.name === "read_from_postgres",
-  )?.enabled;
-
-  // PostgreSQL data
-  const { data: sqlBrewsList } = useSuspenseQuery(
+  const { data: brewsList } = useSuspenseQuery(
     brewsQueryOptions(user?.uid ?? ""),
   );
-  const { data: sqlEspressoList } = useSuspenseQuery(
+  const { data: espressosList } = useSuspenseQuery(
     espressosQueryOptions(user?.uid ?? ""),
   );
 
-  // Firestore data
-  const filters = useMemo(() => [orderBy("date", "desc"), limit(30)], []);
-
-  const brewQuery = useCollectionQuery<Brew>("brews", filters);
-  const { list: fbBrewsList, isLoading: brewsLoading } =
-    useFirestoreCollectionRealtime<Brew>(brewQuery);
-
-  const espressoQuery = useCollectionQuery<Espresso>("espresso", filters);
-  const { list: fbEspressoList, isLoading: espressoLoading } =
-    useFirestoreCollectionRealtime<Espresso>(espressoQuery);
-
-  const beansFilters = useMemo(() => [orderBy("roastDate", "desc")], []);
-  const beansQuery = useCollectionQuery<Beans>("beans", beansFilters);
-  const { list: fbBeansList, isLoading: beansLoading } =
-    useFirestoreCollectionRealtime<Beans>(beansQuery);
-
-  if (brewsLoading || espressoLoading || beansLoading) {
+  if (!brewsList || !espressosList) {
     return null;
   }
 
@@ -109,16 +67,9 @@ function DrinksPage() {
         </li>
       </ul>
 
-      {readFromPostgres ? (
-        <DrinksListPostgres
-          drinks={mergePostgres(sqlBrewsList ?? [], sqlEspressoList ?? [])}
-        />
-      ) : (
-        <DrinksListFirebase
-          drinks={mergeFirebase(fbBrewsList, fbEspressoList)}
-          beansList={fbBeansList}
-        />
-      )}
+      <DrinksListPostgres
+        drinks={mergePostgres(brewsList ?? [], espressosList ?? [])}
+      />
     </>
   );
 }
