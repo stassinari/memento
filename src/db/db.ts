5b7f52ed9b57a0cf40971d1dd9c8715a7c0fc300
Saverio Tassinari <saverio.tassinari@gmail.com>
2026-02-22 22:44:09 +0000

    yeet packages

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,4 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-// eslint-disable-next-line
 export const db = drizzle(process.env.DATABASE_URL!, { schema });
